3244a00a56716bc50903cb7417efb96ce6c2b7bb
David Silva <contacto@dvidsilva.com>
2016-12-14 14:31:13 -0800

    iterating over results

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Github from './github';
 
 class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    loading: true,
+    repos: [],
+  }
+} 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>David Silva's Repos</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="results">
+          {this.renderResults()}
+        </div>
       </div>
     );
   }
+
+  renderResults = () => {   
+    const results = this.state.repos.map((result, idx) =>
+      <div className="repo">
+        {result.name}
+      </div>
+    );
+    return this.state.loading ? <div>Loading...</div> : results;
+  }
+
+  componentDidMount() {
+
+    Github.get(`dvidsilva`).then(data => {
+      console.log(data);
+      this.setState({loading: false});
+      this.setState({repos: data});
+    });
+  }
+
 }
 
 export default App;
